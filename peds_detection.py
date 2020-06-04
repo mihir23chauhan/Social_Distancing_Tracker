5c3b056bf2d2b850abe2867c1316b7cb14084979
divyanshu0x16 <divyanshu22.dm@gmail.com>
2020-06-04 23:15:55 +0530

    Detects distance now

--- peds_detection.py
+++ peds_detection.py
@@ -10,8 +10,23 @@ from scipy.spatial import distance as dist
 
 confid = 0.5
 thresh = 0.5
-MIN_DIST = 50 # min distance in pixels
+#MIN_DIST = 50 # min distance in pixels
 #Link to video file and where to output
+
+def calibrated_dist(p1, p2):
+    return ((p1[0] - p2[0]) ** 2 + 550 / ((p1[1] + p2[1]) / 2) * (p1[1] - p2[1]) ** 2) ** 0.5
+
+
+def isclose(p1, p2):
+    c_d = calibrated_dist(p1, p2)
+    calib = (p1[1] + p2[1]) / 2
+    if 0 < c_d < 0.15 * calib:
+        return 1
+    elif 0 < c_d < 0.2 * calib:
+        return 2
+    else:
+        return 0
+
 def create_arg_parser():
     parser = argparse.ArgumentParser(description="Path to input and output directory")
 
@@ -98,7 +113,7 @@ while True:
 
                     boxes.append([x, y, int(width), int(height)])
                     confidences.append(float(confidence))
-                    centroids.append((centerX, centerY))
+                    centroids.append([centerX, centerY])
                     classIDs.append(classID)
 
 
@@ -113,34 +128,59 @@ while True:
             r = (confidences[i], (x,y,x+w, y+h), centroids[i])
             results.append(r)
 
-    violate = set()
+    status = [0]*len(results)
+    close_pair = list()
+    s_close_pair = list()
 
     if len(results) >= 2:
         #we extract all centroid from the results and compute the
-        # Euclidian disntace between all pairs. Here we take the pixel distnace
+      
         centroids = np.array([r[2] for r in results])
-        D = dist.cdist(centroids, centroids, metric='euclidean')
 
-        for i in range(0, D.shape[0]):
-            for j in range(i+1, D.shape[1]):
-                if D[i,j] < MIN_DIST:
-                    violate.add(i)
-                    violate.add(j)
+        for i in range(len(centroids)):
+            for j in range(len(centroids)):
 
+                dist = isclose(centroids[i], centroids[j])
+
+                if dist  == 1:
+                    close_pair.append([centroids[i], centroids[j]])
+                    status[i] = 1
+                    status[j] = 1
+                elif dist == 2:
+                    s_close_pair.append([centroids[i], centroids[j]])          
+                    if status[i] != 1:
+                        status[i] = 2
+                    if status[j] != 1:
+                        status[j] = 2
+
+    low_risk_p = status.count(2)
+    high_risk_count = status.count(1)
+    safe_p = status.count(0)          
+
+    iter = 0 
     for (i, (prob, bbox, centroid)) in enumerate(results):
         (startX, startY, endX, endY) = bbox
-        (cX, cY) = centroid
-        color = (0, 255, 0)
-
-        if i in violate:
+        
+        if status[iter] == 1:
             color = (0, 0, 255)
+            cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+        elif status[iter] == 0:
+            color = (0, 255, 0)
+            cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+        else:
+            color = (0, 165, 255)
+            cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+            
+        iter += 1
 
-        cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
-        cv2.circle(frame, (cX,cY), 5, color, 1)
+    # Connecting Lines
+    for i in close_pair:
+        cv2.line(frame, tuple(i[0]), tuple(i[1]), (0,0, 255), 2)
+    for i in s_close_pair:
+        cv2.line(frame, tuple(i[0]), tuple(i[1]), (0,255, 255), 2)
 
-    text = "Total Violations: {}".format(len(violate))
+    text = "Total Violations: {}".format(high_risk_count+low_risk_p)
     cv2.putText(frame, text, (10, frame.shape[0] -25) , cv2.FONT_HERSHEY_SIMPLEX, 0.85, (0,0, 255), 4)
-
 
     if writer is None:
         writer = cv2.VideoWriter(str(parsed_args.outputDirectory), fourcc, 30, (frame.shape[1], frame.shape[0]), True)
